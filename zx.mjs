ad3ada5ffe4f5386582927689a031d7bad240079
Gvozd <gvozdev.viktor@gmail.com>
2021-06-13 00:50:14 +0300

    что-то, где-то, кака-то завелось

--- zx.mjs
+++ zx.mjs
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 
 // Copyright 2021 Google LLC
-// 
+//
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
-// 
+//
 //     https://www.apache.org/licenses/LICENSE-2.0
-// 
+//
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -173,7 +173,7 @@ function transformMarkdown(source) {
 
 async function compile(input) {
   let v = $.verbose
-  $.verbose = false
+  $.verbose = true
   let tsc = $`npm_config_yes=true npx -p typescript tsc --target esnext --lib esnext --module esnext --moduleResolution node ${input}`
   $.verbose = v
 
